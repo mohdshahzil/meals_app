ed23a5c4e42f8e8182665f1ad7149d3b1a2ab725
mohdshahzil <theshahzil@gmail.com>
2024-03-11 20:05:49 +0530

    displaying category items

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals_app/models/category.dart';
+
+const availableCategories = [
+  Categoryy(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Categoryy(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Categoryy(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Categoryy(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Categoryy(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Categoryy(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Categoryy(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Categoryy(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Categoryy(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Categoryy(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+];
